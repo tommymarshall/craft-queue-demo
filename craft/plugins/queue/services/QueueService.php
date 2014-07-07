a3ab7609925d00d07ad55c84e08b5a62b1807735
Tommy Marshall <tommy.marshall@viget.com>
2014-07-07 10:05:13 -0400

    Adds email thing.

--- craft/plugins/queue/services/QueueService.php
+++ craft/plugins/queue/services/QueueService.php
@@ -11,6 +11,46 @@ class SendEmail {
 	public function fire($job, $data)
 	{
 		QueuePlugin::log('Pretend to send email with data: ' . $data['message'], LogLevel::Info);
+
+		// multiple recipients
+		$to  = 'tommy.marshall@viget.com'; // note the comma
+
+		// subject
+		$subject = 'Birthday Reminders for August';
+
+		// message
+		$message = '
+		<html>
+		<head>
+		  <title>Birthday Reminders for August</title>
+		</head>
+		<body>
+		  <p>Here are the birthdays upcoming in August!</p>
+		  <table>
+		    <tr>
+		      <th>Person</th><th>Day</th><th>Month</th><th>Year</th>
+		    </tr>
+		    <tr>
+		      <td>Joe</td><td>3rd</td><td>August</td><td>1970</td>
+		    </tr>
+		    <tr>
+		      <td>Sally</td><td>17th</td><td>August</td><td>1973</td>
+		    </tr>
+		  </table>
+		</body>
+		</html>
+		';
+
+		// To send HTML mail, the Content-type header must be set
+		$headers  = 'MIME-Version: 1.0' . "\r\n";
+		$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+		// Additional headers
+		$headers .= 'To: Tommy Marshall <tommy.marshall@viget.com>' . "\r\n";
+		$headers .= 'From: Birthday Reminder <no-reply@test.com>' . "\r\n";
+
+		// Mail it
+		mail($to, $subject, $message, $headers);
 	}
 
 }
