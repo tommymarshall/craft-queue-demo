7eb6a26116d77cca248989da72e0986f7a1034bd
Tommy Marshall <tommy.marshall@viget.com>
2014-07-07 10:19:42 -0400

    Adds namesspace.

--- craft/plugins/queue/services/QueueService.php
+++ craft/plugins/queue/services/QueueService.php
@@ -8,11 +8,6 @@ use \Illuminate\Queue\Capsule\Manager as Queue;
 
 class SendEmail {
 
-	public function __construct()
-	{
-		QueuePlugin::log('CONSTRUCT!', LogLevel::Info, true);
-	}
-
 	public function fire($job, $data)
 	{
 		QueuePlugin::log('Pretend to send email with data (INFO): ' . $data['message'], LogLevel::Info, true);
@@ -52,7 +47,7 @@ class QueueService extends BaseApplicationComponent
 	public function run()
 	{
 		$message = 'This is my awesome message';
-		Queue::push('SendEmail', array('message' => $message));
+		Queue::push('Craft\SendEmail', array('message' => $message));
 	}
 
 	public function marshal()
